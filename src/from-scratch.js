672befda7a53841f14f9a2c4f4356528cce3344b
anthonysalas11 <anthony.salas.ts@gmail.com>
2026-01-07 10:37:08 -0500

    done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -15,18 +15,20 @@
 
 const petJudger = (petBreed, petName) => {
   // Your code here
-  if (petBreed && petName === "") {
-    console.log('Missing information. Please provide a valid pet.')
-  } else if (petBreed === 'cat') {
-    console.log(`I love cats! ${petName} is so cute!`)
+  
+
+  if (!petBreed || !petName ) {
+    console.log ('Missing information. Please provide a valid pet.')
   } else if (petBreed === 'dog') {
-    console.log(`I love dogs! ${petName} is so cute!`)
+    console.log (`I love dogs! ${petName} is so cute!`)
+  } else if (petBreed === 'cat') {
+    console.log (`I love cats! ${petName} is so cute!`)
   } else if (petBreed === 'turtle') {
-    console.log(`Who doesn't love a good turtle? ${petName} is the tops.`)
+    console.log (`Who doesn't love a good turtle? ${petName} is the tops.`)
   } else if (petBreed === 'snake') {
-    console.log(`Not a fan, please take ${petName} and leave.`)
-  } else {
-    console.log(`What an...interesting pet.`)
+    console.log (`Not a fan, please take ${petName} and leave.`)
+  }else {
+    console.log (`What an...interesting pet.`)
   }
 };
 
@@ -36,6 +38,14 @@ const petJudger = (petBreed, petName) => {
 
 const loopFromOneUpToAnother = (firstNum, secondNum) => {
   // Your code here
+ //we are taking a starting number and then counting up until but not including our ending number
+ for (let i = firstNum; i < secondNum;i++){
+ if (firstNum === secondNum || firstNum > secondNum) {
+  return 
+ }
+  console.log(i)
+ }
+    
 };
 
 // ============================================
@@ -44,6 +54,12 @@ const loopFromOneUpToAnother = (firstNum, secondNum) => {
 
 const shoutEveryLetterForLoop = (str) => {
   // Your code here
+  for(let i = 0; i < str.length; i++){
+    
+    console.log(`${str[i].toUpperCase()}!`)
+  }
+
+
 };
 
 // ============================================
@@ -52,6 +68,25 @@ const shoutEveryLetterForLoop = (str) => {
 
 const letterCaseCounts = (str) => {
   // Your code here
+  const counts = {
+    uppercase:0,
+    lowercase:0,
+    neither: 0
+
+  }
+
+  for (let i = 0; i < str.length; i++){
+    const char = str[i]
+   if(char >= "a" && char <= "z"){
+    counts.lowercase++
+   }else if (char >= "A" && char <= "Z"){
+    counts.uppercase++
+   }else {
+    counts.neither++
+   }
+  }
+
+  return counts
 };
 
 // ============================================
@@ -60,7 +95,39 @@ const letterCaseCounts = (str) => {
 
 const getNamesOfGreedyGnomes = (gnomes) => {
   // Your code here
+  const greedyNames = []
+for (let i = 0; i < gnomes.length; i++) {
+  if(gnomes[i].stolenDecorations.length > 1){
+    greedyNames.push(gnomes[i].name)
+  }
+}
+
+return greedyNames
 };
+
+const sumBetweenIndexes = (nums, start = 0, end = nums.length) => {
+
+  if (start < 0) {
+    start = 0;
+  }
+
+ 
+  if (end > nums.length) {
+    end = nums.length;
+  }
+
+  
+  if (start === end) {
+    return nums[start];
+  }
+
+  let sum = 0;
+  for (let i = start; i < end; i++) {
+    sum += nums[i];
+  }
+
+  return sum;
+}
 
 // ============================================
 // Exports
